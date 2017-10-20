b6fa4987b03ea1e46fb5045476f877fa796c688b
AravinthDiliban <aravinthdiliban5@gmail.com>
2017-10-20 14:40:00 +0530

    Update OddorEvenorZeroooo.py

--- OddorEvenorZeroooo.py
+++ OddorEvenorZeroooo.py
@@ -3,7 +3,5 @@
 number = input() #Getting the user input
 if number%2==1 :     #Conditional statement to check whether the number is even or odd
     print "Odd number"
-elif number==0 :
-    print "Zerooooooo"
-else :
+elif number%2==0 :
     print "Even"
