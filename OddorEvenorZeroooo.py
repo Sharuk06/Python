4e6618583cdcf0fc79a1d8d12e41c83d923df54e
AravinthDiliban <aravinthdiliban5@gmail.com>
2017-10-20 14:38:30 +0530

    Create OddorEvenorZeroooo.py

---
+++ OddorEvenorZeroooo.py
@@ -0,0 +1,9 @@
+#SOURCE CODE TO FIND WHETHER THE NUMBER IS EVEN OR ODD
+
+number = input() #Getting the user input
+if number%2==1 :     #Conditional statement to check whether the number is even or odd
+    print "Odd number"
+elif number==0 :
+    print "Zerooooooo"
+else :
+    print "Even"
