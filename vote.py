3371e576993e4657158d5e2a7233320081b2ef8d
AravinthDiliban <aravinthdiliban5@gmail.com>
2017-10-20 14:15:48 +0530

    vote.py

---
+++ vote.py
@@ -0,0 +1,6 @@
+#SOURCE CODE TO FIND WHETHER THE PERSON IS ELIGIBLE TO VOTE
+age = input() #Getting the user input
+if age>=20:     #Conditional statement to check whether the person is eligible to vote
+    print "Eligible"
+else :
+    print "Not Eligible"
