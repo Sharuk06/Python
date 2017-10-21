08178bbcddd08c4364387b17f741b7a88d731137
AravinthDiliban <aravinthdiliban5@gmail.com>
2017-10-21 09:59:20 +0530

    Create comprehension.py

---
+++ list/comprehension.py
@@ -0,0 +1,11 @@
+#SOURCE CODE TO IMPLEMENT LIST COMPREHENSION
+
+#Declaration and initialization of list
+jarvis = ['Aravinth','Diliban','Deepak','Babu','Jaidheep']
+
+#List Comprehension
+jarvisupper = [x.upper() for x in jarvis]
+
+#Printing the list
+print jarvisupper
+
