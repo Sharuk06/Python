238997c99ccd10aa15d6257d40caeee59f8c4970
AravinthDiliban <aravinthdiliban5@gmail.com>
2017-10-21 10:23:57 +0530

    Updates on comment

--- FileHandling/ConvertStringToInteger.py
+++ FileHandling/ConvertStringToInteger.py
@@ -1,6 +1,9 @@
-#SOURCE CODE TO IMPLEMENT FILE HANDLING
+#Python V3 SOURCE CODE TO IMPLEMENT FILE HANDLING
 
+#Opening a file in R+ mode
 file = open("f:\\a.txt","r+")
+#Reading the contents of the file
 content = file.read()
+#List Comprehension 
 list = [int(x) for x in content.split()]
 print list
